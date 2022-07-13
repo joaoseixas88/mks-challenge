5d8906a638d26d59c321d3d47fbbec5c779c40ad
João Seixas <joaoseixas88@gmail.com>
2022-07-13 20:57:19 -0300

    home screen ready

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -8,7 +8,7 @@ declare module "styled-components" {
 const theme = {
   colors: {
     primaryBlue: "#0F52BA",
-    // bgColor: "#F9F9F9",
+    bgColor: "#F9F9F9",
     shape: "#FFFFFF",
     primaryBlack: "#000000",
     secondaryBlack: "#373737",
